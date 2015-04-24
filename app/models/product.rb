318779e1f1781bafeddaea1b50ecb227e7517659
Monica Olinescu <monica.olinescu@gmail.com>
2015-04-24 11:52:59 -0700

    Display the product price (second attempt)
    We moved the ruby code that formats the product price to the model.
    Better, but still not ideal,

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,4 +4,12 @@ class Product < ActiveRecord::Base
 
   validates :name, presence: true
   validates :price_in_cents, presence: true
+
+  # We could use a method on the model to display the price to the user
+  # but this is not ideal, because the model's job is to work with data.
+  # The model should not have to worry about presentation / ui.
+  # The best place for this code is a view helper
+  def pretty_price
+    "$#{price_in_cents / 100.0}"
+  end
 end
