9367f06bb59cf3b2d0c801dce0e535ddebfbf07d
Monica Olinescu <monica.olinescu@gmail.com>
2015-08-14 09:42:20 -0700

    Add validations for product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ActiveRecord::Base
   has_many :orders
   has_many :users, through: :orders
+
+  validates :name, presence: true
+  validates :price_in_cents, presence: true, numericality: { only_integer: true, greater_than: 0 }
 end
