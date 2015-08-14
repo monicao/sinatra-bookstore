9481a4e9add0210b88070c4a0a9edc61a87b9b72
Monica Olinescu <monica.olinescu@gmail.com>
2015-08-14 10:51:45 -0700

    Use helper methods for currency and current_user

--- app/actions.rb
+++ app/actions.rb
@@ -1,14 +1,31 @@
 # app/actions.rb
+helpers do
+  def cents_to_dollars(cents)
+    cents / 100.0
+  end
+
+  def current_user
+    @current_user = User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def require_user
+    if !current_user
+      redirect '/'
+    end
+  end
+end
 
 get '/' do
   erb :'home'
 end
 
 get '/admin/products/new' do
+  require_user
   erb :'products/new', layout: :admin
 end
 
 post '/admin/products' do
+  require_user
   @product = Product.create(name: params[:name], price_in_cents: params[:price_in_cents])
   if @product.persisted?
     redirect '/admin/products'
@@ -19,6 +36,7 @@ post '/admin/products' do
 end
 
 get '/admin/products' do
+  require_user
   @products = Product.all
   erb :'products/index', layout: :admin
 end
@@ -32,5 +50,6 @@ end
 # It doesn't really check the email and password, just logs in the user with id=1
 # Also it does not follow the REST standard. Should be POST instead of GET.
 get '/login' do
-  session[:user_id] = 1
+  session[:user_id] = User.first.id
+  redirect '/'
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -4,4 +4,8 @@ class Product < ActiveRecord::Base
 
   validates :name, presence: true
   validates :price_in_cents, presence: true, numericality: { only_integer: true, greater_than: 0 }
+
+  # def dollars
+  #   self.price_in_cents / 100.0
+  # end
 end
